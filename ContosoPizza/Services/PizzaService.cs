1ac95aa7849c3e4bce7b2171da1af34904a138a8
yunlong.wang <ylwang9627@foxmail.com>
2024-06-06 16:46:37 +0800

    #0 imp pizzaService

--- ContosoPizza/Services/PizzaService.cs
+++ ContosoPizza/Services/PizzaService.cs
@@ -14,33 +14,68 @@ public class PizzaService
 
     public IEnumerable<Pizza> GetAll()
     {
-       return _context.Pizzas
-            .AsNoTracking()
-            .ToList();
+        return _context.Pizzas
+             .AsNoTracking()
+             .ToList();
     }
 
     public Pizza? GetById(int id)
     {
-        throw new NotImplementedException();
+        return _context.Pizzas
+            .Include(p => p.Sauce)
+            .Include(p => p.Toppings)
+            .AsNoTracking()
+            .SingleOrDefault(p => p.Id == id);
     }
 
     public Pizza? Create(Pizza newPizza)
     {
-        throw new NotImplementedException();
+        _context.Pizzas.Add(newPizza);
+        _context.SaveChanges();
+
+        return newPizza;
     }
 
     public void AddTopping(int PizzaId, int ToppingId)
     {
-        throw new NotImplementedException();
+        var pizzaToUpdate = _context.Pizzas.Find(PizzaId);
+        var toppingToAdd = _context.Toppings.Find(ToppingId);
+
+        if (pizzaToUpdate is null || toppingToAdd is null)
+        {
+            throw new InvalidOperationException("pizza or topping doesn't exist.");
+        }
+
+        if (pizzaToUpdate.Toppings is null)
+        {
+            pizzaToUpdate.Toppings = new List<Topping>();
+        }
+
+        pizzaToUpdate.Toppings.Add(toppingToAdd);
+        _context.SaveChanges();
     }
 
     public void UpdateSauce(int PizzaId, int SauceId)
     {
-        throw new NotImplementedException();
+        var pizzaToUpdate = _context.Pizzas.Find(PizzaId);
+        var sauceToUpdate = _context.Sauces.Find(SauceId);
+
+        if (sauceToUpdate is null || pizzaToUpdate is null)
+        {
+            throw new InvalidOperationException("pizza or sauce doesn't exist.");
+        }
+        pizzaToUpdate.Sauce = sauceToUpdate;
+
+        _context.SaveChanges();
     }
 
     public void DeleteById(int id)
     {
-        throw new NotImplementedException();
+        var pizzaToDelete = _context.Pizzas.Find(id);
+        if (pizzaToDelete is not null)
+        {
+            _context.Pizzas.Remove(pizzaToDelete);
+            _context.SaveChanges();
+        }
     }
 }
